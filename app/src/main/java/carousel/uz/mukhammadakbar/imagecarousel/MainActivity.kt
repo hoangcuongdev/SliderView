d161fbe3e060015be11f370dffb0b4ab3ad679dd
rafiqov <muhammadakbar_rafiqov@mail.ru>
2018-07-06 15:48:49 +0500

    glide added

--- app/src/main/java/carousel/uz/mukhammadakbar/imagecarousel/MainActivity.kt
+++ app/src/main/java/carousel/uz/mukhammadakbar/imagecarousel/MainActivity.kt
@@ -1,7 +1,9 @@
 package carousel.uz.mukhammadakbar.imagecarousel
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initCarousel()
+        initCarousels()
     }
 
     private fun initCarousel() {
-        carousel.addImage(R.drawable.nature1)
-        carousel.addImage(R.drawable.nature2)
-        carousel.addImage(R.drawable.nature3)
+        carousel.addImage(imageUrl = "http://apibazarway.wienerdeming.com/media/file/image/2018-06/0d02aea0-71d5-4c51-adb4-e4ea2007e27e.jpg.80x80_q85_crop.jpg")
+        carousel.addImage(imageUrl = "http://apibazarway.wienerdeming.com/media/file/image/2018-06/0d02aea0-71d5-4c51-adb4-e4ea2007e27e.jpg.80x80_q85_crop.jpg")
+        carousel.addImage(imageUrl = "http://apibazarway.wienerdeming.com/media/file/image/2018-05/f3b193f1-7b04-4034-b29a-861d1b6f3ebb.jpg.80x80_q85_crop.jpg")
     }
 
+    private fun initCarousels() {
+        carousel.addImage(drawable = ContextCompat.getDrawable(applicationContext, R.drawable.nature1))
+        carousel.addImage(drawable = ContextCompat.getDrawable(applicationContext, R.drawable.nature2))
+        carousel.addImage(drawable = ContextCompat.getDrawable(applicationContext, R.drawable.nature3))
+    }
 }
